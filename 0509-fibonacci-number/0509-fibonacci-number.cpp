965104445a62cbdc5d4d925599944220cad17ce3
Sayan Adhikary <timepasswithsa@gmail.com>
2026-08-09 23:06:01 +0530

    Time: 0 ms (100%), Space: 8.1 MB (19.89%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    int solve(int n) {
+
+        if (n <= 1)
+            return n;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
+    }
+
+    int fib(int n) {
+        dp.resize(n + 1, -1);
+        return solve(n);
+    }
+};
