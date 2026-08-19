bf5de9cd7e75524f4b626e84fc2ec53ed2fb4661
Sayan Adhikary <timepasswithsa@gmail.com>
2026-08-19 13:03:28 +0530

    Time: 0 ms (100%), Space: 17.6 MB (63.8%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,48 +1,15 @@
 // ============================================================
-// 2. MEMOIZATION
+// 3. TABULATION
 //
 // State:
 // dp[i][tranNo]
 //
 // Time: O(n * k)
-// Space: O(n * k) DP + O(n) recursion stack
-//       = O(n * k)
+// Space: O(n * k)
 // ============================================================
 
 class Solution {
 public:
-
-    int stock(int i, int tranNo,
-              vector<int>& prices,
-              int k,
-              vector<vector<int>>& dp) {
-
-        // Base case
-        if(i == prices.size() || tranNo == 2 * k)
-            return 0;
-
-        // Already calculated
-        if(dp[i][tranNo] != -1)
-            return dp[i][tranNo];
-
-        int profit = 0;
-
-        if(tranNo % 2 == 0) {
-
-            // BUY or SKIP
-            profit = max(-prices[i] + stock(i + 1, tranNo + 1, prices, k, dp),
-                                0 + stock(i + 1,tranNo,prices,k,dp));
-
-        }
-        else {
-
-            // SELL or SKIP
-            profit = max(prices[i] + stock(i + 1,tranNo + 1,prices,k,dp),
-                                0 + stock(i + 1,tranNo,prices,k,dp));
-        }
-
-        return dp[i][tranNo] = profit;
-    }
 
     int maxProfit(int k, vector<int>& prices) {
 
@@ -50,27 +17,45 @@ public:
 
         // dp[i][tranNo]
         vector<vector<int>> dp(
-            n,
-            vector<int>(2 * k, -1)
+            n + 1,
+            vector<int>(2 * k + 1, 0)
         );
 
-        return stock(0, 0, prices, k, dp);
+        // dp[n][...] = 0
+        // Already initialized to 0.
+        //
+        // tranNo = 2*k is also 0.
+        
+        for(int i = n - 1; i >= 0; i--) {
+
+            for(int tranNo = 2 * k - 1;
+                tranNo >= 0;
+                tranNo--) {
+
+                int profit = 0;
+
+                if(tranNo % 2 == 0) {
+
+                    // BUY or SKIP
+                    profit = max(
+                        -prices[i] + dp[i + 1][tranNo + 1],
+                        0 + dp[i + 1][tranNo]
+                    );
+
+                }
+                else {
+
+                    // SELL or SKIP
+                    profit = max(
+                        prices[i] + dp[i + 1][tranNo + 1],
+                        0 + dp[i + 1][tranNo]
+                    );
+                }
+
+                dp[i][tranNo] = profit;
+            }
+        }
+
+        return dp[0][0];
     }
 };
-
-//              tranNo
-//           0    1    2    3
-//        ┌────┬────┬────┬────┐
-// day 0  │ -1 │ -1 │ -1 │ -1 │
-//        ├────┼────┼────┼────┤
-// day 1  │ -1 │ -1 │ -1 │ -1 │
-//        ├────┼────┼────┼────┤
-// day 2  │ -1 │ -1 │ -1 │ -1 │
-//        ├────┼────┼────┼────┤
-// day 3  │ -1 │ -1 │ -1 │ -1 │
-//        ├────┼────┼────┼────┤
-// day 4  │ -1 │ -1 │ -1 │ -1 │
-//        └────┴────┴────┴────┘
-
-//         ↓    ↓    ↓    ↓
-//        BUY  SELL BUY  SELL
