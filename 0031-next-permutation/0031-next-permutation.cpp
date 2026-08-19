de2a278eb5a74310d45d7801a3f30c42e0c73084
Sayan Adhikary <timepasswithsa@gmail.com>
2026-08-19 20:42:19 +0530

    Time: 0 ms (100%), Space: 15.7 MB (82%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int pivot = -1;
+
+
+        for(int i=n-2; i >= 0; i--){
+
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+            if(pivot == -1){
+                reverse(nums.begin(), nums.end());
+                return;
+            } 
+            // Step 2: Find rightmost element just greater than pivot element
+            for (int i = n - 1; i > pivot; i--) {
+                if (nums[i] > nums[pivot]) {
+                    swap(nums[i], nums[pivot]);
+                    break;
+                }
+            }
+        // Step 4: Reverse the suffix elements to the right of the pivot
+        reverse(nums.begin() + pivot + 1, nums.end());
+        }
+};
