bc1b8a0841efebd05e197bb3139559247e0abc73
Sayan Adhikary <timepasswithsa@gmail.com>
2026-08-19 13:02:33 +0530

    Time: 14 ms (31.86%), Space: 17.7 MB (62.44%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,59 @@
+// ============================================================
+// 2. MEMOIZATION
+//
+// State:
+// dp[i][tranNo]
+//
+// Time: O(n * k)
+// Space: O(n * k) DP + O(n) recursion stack
+//       = O(n * k)
+// ============================================================
+
+class Solution {
+public:
+
+    int stock(int i, int tranNo,
+              vector<int>& prices,
+              int k,
+              vector<vector<int>>& dp) {
+
+        // Base case
+        if(i == prices.size() || tranNo == 2 * k)
+            return 0;
+
+        // Already calculated
+        if(dp[i][tranNo] != -1)
+            return dp[i][tranNo];
+
+        int profit = 0;
+
+        if(tranNo % 2 == 0) {
+
+            // BUY or SKIP
+            profit = max(-prices[i] + stock(i + 1, tranNo + 1, prices, k, dp),
+                                0 + stock(i + 1,tranNo,prices,k,dp));
+
+        }
+        else {
+
+            // SELL or SKIP
+            profit = max(prices[i] + stock(i + 1,tranNo + 1,prices,k,dp),
+                                0 + stock(i + 1,tranNo,prices,k,dp));
+        }
+
+        return dp[i][tranNo] = profit;
+    }
+
+    int maxProfit(int k, vector<int>& prices) {
+
+        int n = prices.size();
+
+        // dp[i][tranNo]
+        vector<vector<int>> dp(
+            n,
+            vector<int>(2 * k, -1)
+        );
+
+        return stock(0, 0, prices, k, dp);
+    }
+};
