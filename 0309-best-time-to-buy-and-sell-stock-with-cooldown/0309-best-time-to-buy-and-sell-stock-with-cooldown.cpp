ed0fc363aeb4a8f2bc1642bcf66c38a4b3f771af
Sayan Adhikary <timepasswithsa@gmail.com>
2026-08-19 19:14:35 +0530

    Time: 0 ms (100%), Space: 15.1 MB (86.93%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -3,16 +3,22 @@ public:
 //TABULATION
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n+2, vector<int>(2,0));
-        
+
+        vector<int>front2(2,0);
+        vector<int>front1(2,0);
+        vector<int>curr(2,0);
+
         for(int i = n-1; i >= 0; i--){
             
-            dp[i][1] = max(-prices[i] + dp[i+1][0],
-                                0 + dp[i+1][1]);
+            curr[1] = max(-prices[i] + front1[0],
+                                0 + front1[1]);
 
-            dp[i][0] = max(prices[i] + dp[i+2][1],
-                                0 + dp[i+1][0]);
+            curr[0] = max(prices[i] + front2[1],
+                                0 + front1[0]);
+
+            front2 = front1;
+            front1 = curr;
         }
-        return dp[0][1];
+        return curr[1];
     }
 };
