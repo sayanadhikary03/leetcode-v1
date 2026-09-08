3bf8c736fe2d99d089dee9e2cf736ee73dd6046e
Sayan Adhikary <timepasswithsa@gmail.com>
2026-09-08 13:15:34 +0530

    Time: 0 ms (100%), Space: 10.9 MB (35.01%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -6,38 +6,62 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    // -----------------Morris Inorder Traversal
+    // ------------------------------------------Time: O(n) | Space: O(1)
+
+    // No left → visit → go right
+
+    // Has left → find predecessor
+    //      → create thread → go left
+
+    // Come back through thread
+    //      → remove thread → visit → go right
+
+
     vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr = root;
 
-        vector<int> in;
-
-        stack<TreeNode*> st;
-
-        TreeNode* node = root;
-
-        while(node != NULL || !st.empty()) {
-
-            // Go LEFT
-            while(node != NULL) {
-                st.push(node);
-                node = node->left;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                // No left subtree → visit current node
+                ans.push_back(curr->val);
+                // Move to right subtree
+                curr = curr->right;
             }
+            else {
+                // Find inorder predecessor:
+                // rightmost node in the left subtree
+                // Example: for 5, its predecessor may be 4
+                TreeNode* IP = curr->left;
+                while (IP->right != NULL && IP->right != curr) {
+                    IP = IP->right;
+                }
 
-            // Take node
-            node = st.top();
-            st.pop();
-
-            // Visit node
-            in.push_back(node->val);
-
-            // Go RIGHT
-            node = node->right;
+                if (IP->right == NULL) {
+                    // First time reaching curr:
+                    // create a temporary link back to curr
+                    IP->right = curr;
+                    // Go to the left subtree
+                    curr = curr->left;
+                }
+                else {
+                    // Left subtree is already processed:
+                    // remove the temporary link
+                    IP->right = NULL;
+                    // Now visit curr
+                    ans.push_back(curr->val);
+                    // Move to the right subtree
+                    curr = curr->right;
+                }
+            }
         }
-
-        return in;
+        return ans;
     }
 };
